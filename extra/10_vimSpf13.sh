702ca2f50c0eef38831b0b1c00dcc1e333e5d458
Daryl Gubler <daryl@dev88.co>
2014-10-19 19:28:44 -0400

    Disabled spf13 until I get better with Vim.

--- extra/10_vimSpf13.sh
+++ extra/10_vimSpf13.sh
@@ -4,21 +4,21 @@ function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
 function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 
-if [[ ! -d "$HOME/.spf13-vim-3" ]]; then
-  e_header "Installing spf13 for vim"
+# if [[ ! -d "$HOME/.spf13-vim-3" ]]; then
+#   e_header "Installing spf13 for vim"
 
-  cd $HOME
+#   cd $HOME
 
-  curl http://j.mp/spf13-vim3 -L -o - | sh
+#   curl http://j.mp/spf13-vim3 -L -o - | sh
 
-fi
+# fi
 
-if [[ -d "$HOME/.spf13-vim-3" ]]; then
-  e_header "Updating spf13 for vim"
+# if [[ -d "$HOME/.spf13-vim-3" ]]; then
+#   e_header "Updating spf13 for vim"
 
-  cd $HOME/.spf13-vim-3/
+#   cd $HOME/.spf13-vim-3/
 
-  git pull
-  vim +BundleInstall! +BundleClean +q
+#   git pull
+#   vim +BundleInstall! +BundleClean +q
 
-fi
+# fi
