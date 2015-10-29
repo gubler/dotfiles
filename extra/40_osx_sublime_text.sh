1563bff295487e44fb8a57e45864376692a5413a
Daryl Gubler <EMAIL>
2015-10-29 11:21:00 -0400

    Added Marked 2 builder for Sublime Text

--- extra/40_osx_sublime_text.sh
+++ extra/40_osx_sublime_text.sh
@@ -7,3 +7,4 @@ echo "Install Sublime Text settings and packages"
 cp -r ${HOME}/.dotfiles/reference/SublimeText/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
 cp -r ${HOME}/.dotfiles/reference/SublimeText/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text*/Installed\ Packages/Package\ Control.sublime-package 2> /dev/null
 cp -r ${HOME}/.dotfiles/reference/SublimeText/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Package\ Control.sublime-settings 2> /dev/null
+cp -r ${HOME}/.dotfiles/reference/SublimeText/Marked 2.sublime-build ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Marked 2.sublime-build 2> /dev/null
