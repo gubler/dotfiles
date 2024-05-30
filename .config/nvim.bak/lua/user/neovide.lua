10f8815bd9f4f409f43e1727609cc04014235f92
Daryl Gubler <daryl@dev88.co>
2024-05-29 20:43:51 -0400

    chore: remove obsolete files

--- .config/nvim.bak/lua/user/neovide.lua
+++
@@ -1,5 +0,0 @@
-if vim.g.neovide then
-    vim.opt.guifont = { "JetBrainsMono_Nerd_Font_Mono", "h18"}
-    vim.opt.linespace = 2
-    vim.g.neovide_cursor_trail_size = 0.2
-end
--- .config/nvim.bak/lua/user/plugins/symbols-outline.lua
+++
@@ -1,6 +0,0 @@
-require('symbols-outline').setup({
-        auto_close = true,
-        show_numbers = true,
-        autofold_depth = 2,
-    })
-
